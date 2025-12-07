8cabcaf892f93644d8d8371ef6dd6968819c4070
YazidDiz <154859867+YazidDiz@users.noreply.github.com>
2025-12-07 14:27:57 +0100

    seeges

--- src/gui/main.tsx
+++ src/gui/main.tsx
@@ -11,5 +11,11 @@ import "./index.scss";
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <App />
+        <div className="credit-footer">
+            By Zetsu / Yazid &mdash; 
+            <a href="https://www.facebook.com/profile.php?id=61576203489525" target="_blank" rel="noopener noreferrer">
+                Facebook
+            </a>
+        </div>
     </React.StrictMode>
 )
